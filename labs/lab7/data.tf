586754af202c11b183fa8710b3cad549a9d2c1f0
joseph <josephkeav95@hotmail.com>
2024-03-19 01:18:21 -0400

    using vault to create resources, issue with hashicorp/vault, not seeing versions matching 1.15.6

---
+++ labs/lab7/data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+data "vault_generic_secret" "aws_access_key_id"{
+  path = "secret/aws_access_key_id"
+}
+
+data "vault_generic_secret" "aws_secret_access_key"{
+  path = "secret/aws_secret_access_key"
+}
